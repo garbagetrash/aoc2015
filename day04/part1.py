8c2a3d7e65c53b8ff1fc628631d213a8ca332e0a
Kyle Harris <kaharrs2@gmail.com>
2017-11-30 23:32:37 -0700

    Added day 4 answers

---
+++ day04/part1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import sys
+import hashlib
+
+
+def find_num(in_file):
+    with open(in_file) as f:
+        in_string = f.read().strip()
+
+    num = 1
+    while True:
+        h = hashlib.md5((in_string + str(num)).encode('utf-8')).hexdigest()
+        if h[:5] == '00000':
+            return num
+        else:
+            num += 1
+
+
+if __name__ == '__main__':
+    output = find_num(sys.argv[1])
+    print('Output: {}'.format(output))
